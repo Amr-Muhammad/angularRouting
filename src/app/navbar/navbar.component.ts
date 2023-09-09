6d152b283db36be511106a2070fd6392cd2be978
Mik00z <amr.casepr.98@gmail.com>
2023-09-09 05:05:54 +0300

    navBar aniamtion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+
+  // test = $('#navbar').text()
+
+  we() {
+
+    if ($("nav").offset()?.top != 0) {
+      $("nav").removeClass('py-4')
+    }
+
+
+  }
 }
+
+
+window.addEventListener('scroll', function () {
+  // if ($("nav").offset()?.top != 0){
+  //   $("nav").removeClass('py-4')
+  // }
+  // else{
+  //   $("nav").addClass('py-4')
+  // }
+
+
+  if ($("nav").offset()?.top == 0) {
+
+    $("nav").stop().animate({
+      paddingTop: '10px',
+      paddingBottom: '10px'
+    }
+      ,
+      {
+        duration: 1,
+        complete: function () {
+          $("nav").animate({
+            paddingTop: '25px',
+            paddingBottom: '25px'
+          },500)
+        }
+      })
+
+  }
+
+  else {
+
+    $("nav").stop().animate({
+      paddingTop: '10px',
+      paddingBottom: '10px'
+    }, 500).removeAttr('style')
+
+  }
+
+});
+
